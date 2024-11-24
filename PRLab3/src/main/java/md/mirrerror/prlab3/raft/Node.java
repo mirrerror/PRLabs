13864641aaf70f0934bd4dc65e8589fda109da32
mirrerror <cayote20@mail.ru>
2024-11-24 16:03:59 +0200

    update the node

--- PRLab3/src/main/java/md/mirrerror/prlab3/raft/Node.java
+++ PRLab3/src/main/java/md/mirrerror/prlab3/raft/Node.java
@@ -2,17 +2,18 @@ package md.mirrerror.prlab3.raft;
 
 import lombok.Getter;
 import lombok.Setter;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 import java.util.Random;
+import java.util.stream.Stream;
 
 @Component
 @Getter
 public class Node {
 
-    private int id;
+    private final int id;
+    private final List<Integer> peers;
 
     @Setter
     private NodeState state;
@@ -21,29 +22,31 @@ public class Node {
 
     private int votedFor;
 
-    private List<Integer> peers;
-
     private int electionTimeout;
 
     @Setter
     private long lastHeartbeat;
 
     public Node() {
+        String nodeId = System.getenv("NODE_ID");
+        if (nodeId == null) {
+            throw new IllegalArgumentException("NODE_ID environment variable is not set");
+        }
+
+        String nodePeers = System.getenv("NODE_PEERS");
+        if (nodePeers == null) {
+            throw new IllegalArgumentException("NODE_PEERS environment variable is not set");
+        }
+
+        this.id = Integer.parseInt(nodeId);
+        this.peers = Stream.of(nodePeers.split(","))
+                .map(Integer::parseInt)
+                .toList();
         this.state = NodeState.FOLLOWER;
         this.currentTerm = 0;
         this.votedFor = -1;
         setRandomElectionTimeout();
         this.lastHeartbeat = System.currentTimeMillis();
-    }
-
-    @Value("${node.id}")
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    @Value("${node.peers}")
-    public void setPeers(List<Integer> peers) {
-        this.peers = peers;
     }
 
     public void incrementTerm() {
